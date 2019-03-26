31400a8fc19f8b5378e78e5df42d6e71e59846c6
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-26 08:29:55 +0100

    :sparkles: Add basic loader from CDN

--- loader.ts
+++ loader.ts
@@ -1,1 +1,20 @@
-console.log("Loading...");
+const cdnUrl = "https://secure-agastya-cdn.oswaldlabs.com";
+const request = new XMLHttpRequest();
+request.onreadystatechange = () => {
+  if (4 === request.readyState) {
+    const data = JSON.parse(request.responseText);
+    const scriptUrl = `/agastya.${data.cacheKey}.js`;
+    const script = document.createElement("script");
+    script.setAttribute("data-cacheKey", data.cacheKey);
+    script.setAttribute("data-baseUrl", cdnUrl);
+    script.setAttribute("src", scriptUrl);
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.head ||
+      document.body ||
+      document.documentElement
+    ).appendChild(script);
+  }
+};
+request.open("GET", `${cdnUrl}/meta.json`, false);
+request.send();
